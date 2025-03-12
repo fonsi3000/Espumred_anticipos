7e7481e3238fcdbe62b8a397bd51fa702b526d56
fonsi3000 <luiiscarrascal@gmail.com>
2025-03-12 09:05:27 -0500

    Se optimiza mediante debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use BezhanSalleh\FilamentShield\Facades\FilamentShield;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,5 +30,34 @@ class AppServiceProvider extends ServiceProvider
                 ->kebab()          // Convierte a kebab-case
                 ->toString();      // Convierte a string
         });
+
+        // Monitoreo de consultas SQL en modo debug
+        if (config('app.debug')) {
+            DB::listen(function ($query) {
+                if ($query->time > 100) { // Solo log consultas que tomen más de 100ms
+                    Log::info('Consulta SQL lenta: ' . $query->sql, [
+                        'bindings' => $query->bindings,
+                        'time' => $query->time . 'ms'
+                    ]);
+                }
+            });
+        }
+
+        // Monitoreo de Livewire si está instalado
+        if (class_exists('\Livewire\Livewire')) {
+            \Livewire\Livewire::listen('component.hydrate', function ($component) {
+                Log::debug('Inicio de hidratación del componente: ' . get_class($component), [
+                    'tiempo_inicio' => microtime(true),
+                    'memoria_inicio' => memory_get_usage() / 1024 / 1024 . ' MB'
+                ]);
+            });
+
+            \Livewire\Livewire::listen('component.dehydrate', function ($component) {
+                Log::debug('Fin de deshidratación del componente: ' . get_class($component), [
+                    'tiempo_fin' => microtime(true),
+                    'memoria_fin' => memory_get_usage() / 1024 / 1024 . ' MB'
+                ]);
+            });
+        }
     }
 }
